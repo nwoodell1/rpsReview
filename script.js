8c6ae47aa306ee60bb41d8d06b47d8381b457658
Nathan <nathan.woodell@gmail.com>
2025-06-11 18:48:49 -0500

    have all the logic for deciding the winner and keeping score for one round.

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
 /* get the computer's submission */
 
-function getCompChoice() {
-   var num = Math.random();
-   if (num < 0.3333){return "rock"}
-   else if (num < 0.6666) {return "paper"}
-   else {return "scissors"}
-}
+        
 
-let compChoice = getCompChoice()
-console.log(`the computer throws ${compChoice}`)
 /* get the human's submission */
 
-function getHumanChoice(){
-    var choice = prompt("Rock, Paper, or Scissors?");
-    return choice;
-}
-
-let humanChoice = getHumanChoice();
-
-console.log(`the human chose ${humanChoice}`)
+        
 
 // compare the two
-// notify the human of the winner
+
+        function playRound(){
+
+            var humanScore=0
+            var compScore=0
+            var ties=0
+
+            function getCompChoice() {
+                var num = Math.random();
+                if (num < 0.3333){return "rock"}
+                else if (num < 0.6666) {return "paper"}
+                else {return "scissors"}
+                }
+
+            let compChoice = getCompChoice()
+            console.log(`the computer throws ${compChoice}`)
+
+            function getHumanChoice(){
+                var choice = prompt("Rock, Paper, or Scissors?");
+                return choice;
+                }
+
+            let humanChoice = getHumanChoice();
+
+            console.log(`the human throws ${humanChoice}`)
+            
+            // logic for comparing the choices and keeping score
+                if (humanChoice === "rock" & compChoice === "scissors"){
+                    console.log("Human wins this round!");
+                    humanScore++}
+                else if(humanChoice === "paper" & compChoice === "rock"){
+                    console.log("Human wins this round!");
+                    humanScore++}
+                else if(humanChoice === "scissors" & compChoice === "paper"){
+                    console.log("Human wins this round")
+                    humanScore++}
+                else if(humanChoice===compChoice)
+                    {console.log("Its a tie!");
+                    ties++}
+                else
+                    {console.log("Computer wins this round!")
+                    compScore++;}
+            console.log(`The score is human ${humanScore}, computer ${compScore}, and ${ties} ties.`)
+
+        }
+
+        playRound()
+// play a game
 // update the score
 // replay several times
 
